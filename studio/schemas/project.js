db740aaef2abeff9193445f85ad508d70b8fc5a9
Sandmal <sandmal.cornelius@gmail.com>
2022-03-19 18:20:17 +0100

    Add project schema

---
+++ studio/schemas/project.js
@@ -0,0 +1,42 @@
+export default {
+  name: 'project',
+  title: 'Project',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: (doc) => `${doc.title}`,
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'blockContent',
+    },
+  ],
+
+  preview: {
+    select: {
+      title: 'title',
+      student: 'student.name',
+      media: 'mainImage',
+    },
+  },
+};
