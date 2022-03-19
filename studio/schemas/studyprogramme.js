9dd8de4e928776bcdf92fa44f58766ba15c09b60
Sandmal <sandmal.cornelius@gmail.com>
2022-03-19 18:19:57 +0100

    Add studyprogramme schema

---
+++ studio/schemas/studyprogramme.js
@@ -0,0 +1,49 @@
+export default {
+  name: 'studyprogramme',
+  title: 'Studyprogramme',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'code',
+      code: 'Code',
+      type: 'string',
+    },
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'code',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'array',
+      of: [
+        {
+          title: 'Block',
+          type: 'block',
+          styles: [{ title: 'Normal', value: 'normal' }],
+          lists: [],
+        },
+      ],
+    },
+  ],
+  preview: {
+    select: {
+      title: 'code',
+    },
+  },
+};
