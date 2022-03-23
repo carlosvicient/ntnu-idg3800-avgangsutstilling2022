5064d916f271aefe161fca9f33195464fc9b86ef
Sandmal <sandmal.cornelius@gmail.com>
2022-03-23 11:58:54 +0100

    Import graphql and add query to graphql

--- frontend/src/templates/student.jsx
+++ frontend/src/templates/student.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import {graphql} from 'graphql'
 function Student({ data }) {
     console.log(data);
 
@@ -11,7 +11,7 @@ function Student({ data }) {
 }
 
 export const query = graphql`
-    {
+query ($slug: [String] = "") {
         allSanityStudent(filter: {slug: {current: {in: $slug}}}) {
             edges {
             node {
@@ -23,5 +23,4 @@ export const query = graphql`
         }
     }
 `
-
 export default Student;
