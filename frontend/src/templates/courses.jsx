ea08e3cf1bc17eda332619fc57e62b3b122d70b3
Sandmal <sandmal.cornelius@gmail.com>
2022-03-23 11:58:35 +0100

    Fix destructured some props

--- frontend/src/templates/courses.jsx
+++ frontend/src/templates/courses.jsx
@@ -1,19 +1,16 @@
 import React from "react";
 import { graphql, Link } from "gatsby";
 
-export default function Courses(props) {
-    const data = props.data;
+export default function Courses({data}) {
     const students = data.allSanityStudent.edges;
-    console.log(props);
-
     return (
         <main>
             <h1>Studenter - </h1>
             <p>Paragraf av tekst</p>
-            {students.map(student => (
-                <Link to={`${student.node.slug.current}`} key={student.node.slug.current}>
+            {students.map(({node}) => (
+                <Link to={`${node.slug.current}`} key={node.slug.current}>
                     <div>
-                        <img src={student.node.image._rawAsset._ref} alt={student.node.name} />
+                       {/*  <img src={node.image._rawAsset._ref} alt={node.name} /> */}
                     </div>
                 </Link>
             ))}
