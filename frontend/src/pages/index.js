637fa603cdefc278c059453aaec9cf53aa1e0211
Glenn Hansen <hansenglenneirik@gmail.com>
2022-03-23 16:21:24 +0100

    Comment index.js

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -10,6 +10,19 @@ const IndexPage = ({ data }) => {
     <>
       <Seo title="Home" />
       <h1>Avgangsutstilling</h1>
+
+      {/* Generell informasjon, her må det fylles inn korrekt tekst */}
+      <h2>Om</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+
+      <h2>Når</h2>
+      <p>Dato</p>
+
+      <h2>Hvor</h2>
+      <p>Adresse</p>
+
+
+      {/* Mapper ut hver av kortene som er på siden, linkene er henholdvis /bwu, /bmed og /bixd. Disse kan hardkodes i en navbar */}
       {studyProgrammes.map(programme => (
         <div key={programme.node.code}>
           <h2>{programme.node.name}</h2>
