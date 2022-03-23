d360f35331b203ec3ab6fc182b2c659fb879bca5
jostein-tollefsrud <mail@josteintollefsrud.no>
2022-03-23 18:37:16 +0100

    add StudentCard component

---
+++ frontend/src/components/StudentCard.jsx
@@ -0,0 +1,19 @@
+import { Link } from "gatsby"
+import SanityImage from "gatsby-plugin-sanity-image"
+import * as React from "react"
+
+// Student card displaying image and name of a student, linking to the spesific student
+const StudentCard = ({ node }) => {
+  return (
+    <Link to={`${node.slug.current}`}>
+      <div>
+        <SanityImage asset={node.image._rawAsset} alt="" />
+        <p>
+          {node.name} {node.firstlettersurname}
+        </p>
+      </div>
+    </Link>
+  )
+}
+
+export default StudentCard
--- frontend/src/templates/courses.jsx
+++ frontend/src/templates/courses.jsx
@@ -1,65 +1,63 @@
-import React from "react";
-import { graphql, Link } from "gatsby";
-import SanityImage from "gatsby-plugin-sanity-image";
+import React from "react"
+import { graphql, Link } from "gatsby"
+import SanityImage from "gatsby-plugin-sanity-image"
+import StudentCard from "../components/StudentCard"
 
 // Dette er kun pseudokode for å vise hva som er hva, det må lages komponenter til de forskjellige tingene og legges inn i templaten her
 
-export default function Courses({data}) {
-    const students = data.allSanityStudent.edges;
-    const name = data.allSanityStudent.edges[0].node.studyprogramme.name;
-    console.log(data)
-    const description = data.allSanityStudent.edges[0].node.studyprogramme.description[0].children[0].text;
+export default function Courses({ data }) {
+  const students = data.allSanityStudent.edges
+  const name = data.allSanityStudent.edges[0].node.studyprogramme.name
+  console.log(data)
+  const description =
+    data.allSanityStudent.edges[0].node.studyprogramme.description[0]
+      .children[0].text
 
-    return (
-      <main>
-        {/* Tilbake til forsiden */}
-        <Link to="/">&#8592; Tilbake</Link>
+  return (
+    <main>
+      {/* Tilbake til forsiden */}
+      <Link to="/">&#8592; Tilbake</Link>
 
-        {/* Overskrift og description */}
-        <h1>{name}</h1>
-        <p>{description}</p>
+      {/* Overskrift og description */}
+      <h1>{name}</h1>
+      <p>{description}</p>
 
-        {/* Mapper ut hver student i arrayen med bilde og navn under, denne fungerer som en link */}
-        <h2>Studenter</h2>
-        {students.map(({ node }) => (
-          <Link to={`${node.slug.current}`} key={node.id}>
-            <div>
-              <SanityImage asset={node.image._rawAsset} width={300} alt="" />
-              <p>
-                {node.name} {node.firstlettersurname}.
-              </p>
-            </div>
-          </Link>
-        ))}
-      </main>
-    )
+      {/* Mapper ut hver student i arrayen med bilde og navn under, denne fungerer som en link */}
+      <h2>Studenter</h2>
+      {students.map(({ node }) => (
+        <StudentCard node={node} />
+      ))}
+    </main>
+  )
 }
 
 export const query = graphql`
-    query ($slug: [String] = "") {
-        allSanityStudent(filter: {studyprogramme: {slug: {current: {in: $slug}}}}) {
-            edges {
-                node {
-                    id
-                    name
-                    firstlettersurname
-                    image {
-                        _rawAsset
-                    }
-                    studyprogramme {
-                        title
-                        name
-                        description {
-                            children {
-                                text
-                            }
-                        }
-                    }
-                    slug{
-                        current
-                    }
-                }
+  query ($slug: [String] = "") {
+    allSanityStudent(
+      filter: { studyprogramme: { slug: { current: { in: $slug } } } }
+    ) {
+      edges {
+        node {
+          id
+          name
+          firstlettersurname
+          image {
+            _rawAsset
+          }
+          studyprogramme {
+            title
+            name
+            description {
+              children {
+                text
+              }
             }
+          }
+          slug {
+            current
+          }
         }
+      }
     }
-    `
+  }
+`
