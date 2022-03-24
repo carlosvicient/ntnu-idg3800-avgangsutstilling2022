b670bf4095a48808a98e0ecf762df88852bebc3a
Glenn Hansen <hansenglenneirik@gmail.com>
2022-03-24 13:47:55 +0100

    Add tankestrek

--- frontend/src/components/StudentList.jsx
+++ frontend/src/components/StudentList.jsx
@@ -29,7 +29,7 @@ const StudentList = ({ students, withSorting, studyprogramme }) => {
 
   return (
     <div>
-      <h2>Studenter {studyprogramme && `- ${studyprogramme}`}</h2>
+      <h2>Studenter {studyprogramme && `– ${studyprogramme}`}</h2>
       {withSorting && (
         <button aria-label="sorter" onClick={() => handleClick()}>
           {sorting ? "a - z" : "z - a"}
