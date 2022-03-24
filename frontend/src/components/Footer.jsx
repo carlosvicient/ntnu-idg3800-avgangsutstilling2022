f84ed9f3ec89efb6d936d28788ef5ac223d13028
Glenn Hansen <hansenglenneirik@gmail.com>
2022-03-24 15:34:16 +0100

    Add logo to footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Logo from "../images/ntnu_uten_slagord_svart.png";
 
 const Footer = () => {
   return (
     <footer>
       <div>
-        <span>NTNU Logo</span>
+        <img src={Logo} alt="NTNU Logo" />
         <p>Fakultet for arkitektur og design</p>
         <p>Institutt for design</p>
       </div>
