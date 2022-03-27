9bd51499be28b179987cfdadde2cdf371d6eba6a
Sometimesgood <leonardtube98@gmail.com>
2022-03-27 23:28:29 +0100

    social links + bio card

---
+++ frontend/src/components/Icon.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react"
+import { GrLinkedinOption } from "react-icons/gr"
+import { BsInstagram, BsBehance } from "react-icons/bs"
+
+const Icon = ({ type, ...rest }) => {
+  let icon
+  switch (type) {
+    case "linkedin":
+      icon = <GrLinkedinOption />
+      break
+    case "instagram":
+      icon = <BsInstagram />
+      break
+    case "behance":
+      icon = <BsBehance />
+      break
+    default:
+      icon = null
+      break
+  }
+  return <span {...rest}>{icon}</span>
+}
+
+export default Icon
--- frontend/src/components/SocialLinks.jsx
+++ frontend/src/components/SocialLinks.jsx
@@ -1,30 +1,98 @@
-import React from "react";
+import React from "react"
+import SocialLink from "./SocialLink"
+import { IconContext } from "react-icons"
+import { GrLinkedinOption } from "react-icons/gr"
+import { BsInstagram, BsBehance } from "react-icons/bs"
+import * as styles from "../styles/socialLinks.module.css"
+
+/**Hvis vi alltid skal ha behance, insta og linked in, så funker dette
+ *
+ * Men! hvis vi vil velge hva vi skal presentere så brukes det som er under den
+ * kommenterte koden, se linje 60
+ */
+
+// const SocialLinks = ({ links }) => {
+//   return (
+//     <IconContext.Provider
+//       value={{
+//         className: "shared-class",
+
+//         size: "24px",
+//       }}
+//     >
+//       <div className={styles.allIconsContainer}>
+//         {links.behance ? (
+//           <a href={links.behance} target="_blank" rel="noreferrer">
+//             <div className={styles.iconContainer}>
+//               <BsBehance className={styles.socialLinkIcon} />
+//             </div>
+//           </a>
+//         ) : null}
+//         {links.instagram ? (
+//           <a href={links.instagram} target="_blank" rel="noreferrer">
+//             <div className={styles.iconContainer}>
+//               <BsInstagram className={styles.socialLinkIcon} />
+//             </div>
+//           </a>
+//         ) : null}
+//         {links.linkedin ? (
+//           <a href={links.linkedin} target="_blank" rel="noreferrer">
+//             <div className={styles.iconContainer}>
+//               <GrLinkedinOption className={styles.socialLinkIcon} />
+//             </div>
+//           </a>
+//         ) : null}
+//         {links.portfolio ? (
+//           <a
+//             href={links.portfolio}
+//             target="_blank"
+//             rel="noreferrer"
+//             className={styles.portfolioLink}
+//           >
+//             Se portfolio
+//           </a>
+//         ) : null}
+//       </div>
+//     </IconContext.Provider>
+//   )
+// }
+
+/**Hvis vi vil bestemme hvilke ikoner som vises, så funker dette) */
 
 const SocialLinks = ({ links }) => {
+  console.log(links)
   return (
-    <div>
-      {links.behance ? (
-        <a href={links.behance} target="_blank" rel="noreferrer">
-          Behance
-        </a>
-      ) : null}
-      {links.instagram ? (
-        <a href={links.instagram} target="_blank" rel="noreferrer">
-          Instagram
-        </a>
-      ) : null}
-      {links.linkedin ? (
-        <a href={links.linkedin} target="_blank" rel="noreferrer">
-          LinkedIn
-        </a>
-      ) : null}
-      {links.portfolio ? (
-        <a href={links.portfolio} target="_blank" rel="noreferrer">
-          Se portfolio
-        </a>
-      ) : null}
-    </div>
+    <IconContext.Provider
+      value={{
+        className: "shared-class",
+
+        size: "24px",
+      }}
+    >
+      <div className={styles.allIconsContainer}>
+        {Object.entries(links).map(([name, url]) => {
+          if (name === "portfolio") {
+            return (
+              <a
+                href={url}
+                target="_blank"
+                rel="noreferrer"
+                className={styles.portfolioLink}
+              >
+                Se portfolio
+              </a>
+            )
+          } else {
+            return (
+              <a key={name} href={url} target="_blank" rel="noreferrer">
+                <SocialLink name={name} />
+              </a>
+            )
+          }
+        })}
+      </div>
+    </IconContext.Provider>
   )
 }
 
-export default SocialLinks;
+export default SocialLinks
