eff3aca2af6233f340f54b2643a674139c505e96
LenaAL <lenaamdallarsen@gmail.com>
2022-03-28 15:49:25 +0200

    made css for back link

--- frontend/src/components/BackLink.jsx
+++ frontend/src/components/BackLink.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import { Link } from "gatsby";
 
+// Style
+import * as style from "../styles/backLink.module.css"
+import { MdArrowBackIosNew } from 'react-icons/md';
+import { BsArrowLeft } from 'react-icons/bs';
+
+
 const BackLink = ({ to }) => {
-  return <Link to={to}>&#8592; Tilbake</Link>;
+  return <Link className={style.backLink} to={to}>
+    <MdArrowBackIosNew className={style.mobileArrow} />
+    <BsArrowLeft className={style.desktopArrow} />
+    <span>Tilbake</span>
+  </Link>;
 }
 
 export default BackLink;
