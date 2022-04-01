8add94ae8daa8240ad50d2b07bf17755e4c2e0c0
Glenn Hansen <hansenglenneirik@gmail.com>
2022-04-01 13:29:38 +0200

    Add about page

---
+++ frontend/src/pages/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+function About() {
+    return (
+        <Layout>
+            
+        </Layout>
+    );
+}
+
+export default About;
