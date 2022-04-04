5459201a09b4c8024b03fed43a984e1b23e9c5f5
Glenn Hansen <hansenglenneirik@gmail.com>
2022-04-04 12:31:51 +0200

    Remove unused files

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "gatsby";
-import { StaticImage } from "gatsby-plugin-image";
 import logoDark from "../assets/images/logo-dark.svg";
 import logo from "../assets/images/logo.svg";
 
