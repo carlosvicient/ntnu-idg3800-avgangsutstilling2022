c69529a6354ba511d603c4b39d179fe12b208ebb
Ida <imgjeits@stud.ntnu.no>
2022-04-05 14:11:25 +0200

    fixes after merge

--- frontend/src/components/BioCard.jsx
+++ frontend/src/components/BioCard.jsx
@@ -9,11 +9,16 @@ const BioCard = ({ student }) => {
   return (
     <div className={styles.container}>
       <div className={styles.cardContainer}>
-        {student.name && student.surname &&<h2 className={styles.fullName}>
-          {`${student.name} 
-        ${student.surname.charAt(0)}.`}
-        </h2>
-        {student.studyprogramme.title && <p className={styles.studyProgramme}>{student.studyprogramme.title}</p>}
+        {student.name && student.surname && (
+          <h2 className={styles.fullName}>
+            {`${student.name} ${student.surname.charAt(0)}.`}
+          </h2>
+        )}
+        {student.studyprogramme.title && (
+          <p className={styles.studyProgramme}>
+            {student.studyprogramme.title}
+          </p>
+        )}
         <span>
           <div>
             {student.image && (
@@ -25,7 +30,7 @@ const BioCard = ({ student }) => {
             )}
           </div>
           <div className={styles.bioContainer}>
-            <p className={styles.bio}>{student.bio.map((bio) => (bio.children[0].text))}</p>
+            <p>{student.bio.map(bio => bio.children[0].text)}</p>
           </div>
         </span>
       </div>
--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -19,66 +19,145 @@ function About() {
         <header className={header.header}>
           <h1>Om</h1>
         </header>
-            <section>
-                <h2>Bærekraftansvarlig</h2>
-                <ul>
-                    <li><Link to='/bwu/tom-schrier'>Tom Schrier</Link></li>
-                </ul>
+        <section>
+          <h2>Bærekraftansvarlig</h2>
+          <ul>
+            <li>
+              <Link to="/bwu/tom-schrier">Tom Schrier</Link>
+            </li>
+          </ul>
 
-                <h2>Designansvarlige</h2>
-                <ul>
-                    <li><Link to='/bmed/adam-maximilian-stoffel'>Adam Maximilian Stoffel</Link></li>
-                    <li><Link to='/bmed/amalie-bach'>Amalie Bach</Link></li>
-                    <li><Link to='/bmed/andrea-hauge-sandvik'>Andrea Hauge Sandvik</Link></li>
-                    <li><Link to='/bmed/johanne-lyster-strande'>Johanne Lyster Strande</Link></li>
-                    <li><Link to='/bmed/martine-egeli'>Martine Egeli</Link></li>
-                </ul>
+          <h2>Designansvarlige</h2>
+          <ul>
+            <li>
+              <Link to="/bmed/adam-maximilian-stoffel">
+                Adam Maximilian Stoffel
+              </Link>
+            </li>
+            <li>
+              <Link to="/bmed/amalie-bach">Amalie Bach</Link>
+            </li>
+            <li>
+              <Link to="/bmed/andrea-hauge-sandvik">Andrea Hauge Sandvik</Link>
+            </li>
+            <li>
+              <Link to="/bmed/johanne-lyster-strande">
+                Johanne Lyster Strande
+              </Link>
+            </li>
+            <li>
+              <Link to="/bmed/martine-egeli">Martine Egeli</Link>
+            </li>
+          </ul>
 
-                <h2>Fotoansvarlige</h2>
-                <ul>
-                    <li><Link to='/bmed/johanne-lyster-strande'>Johanne Lyster Strande</Link></li>
-                    <li><Link to='/bmed/marc-daniel-einebakken'>Marc Daniel Einebakken</Link></li>
-                    <li><Link to='/bmed/nora-strom-myklebust'>Nora Strøm Myklebust</Link></li>
-                </ul>
+          <h2>Fotoansvarlige</h2>
+          <ul>
+            <li>
+              <Link to="/bmed/johanne-lyster-strande">
+                Johanne Lyster Strande
+              </Link>
+            </li>
+            <li>
+              <Link to="/bmed/marc-daniel-einebakken">
+                Marc Daniel Einebakken
+              </Link>
+            </li>
+            <li>
+              <Link to="/bmed/nora-strom-myklebust">Nora Strøm Myklebust</Link>
+            </li>
+          </ul>
 
-                <h2>Informasjonsarkitektur-ansvarlige</h2>
-                <ul>
-                    <li><Link to='/bixd/hanna-hernaes'>Hanna Hernæs</Link></li>
-                    <li><Link to='/bixd/ida-emilie-herfoss'>Ida Emilie Herfoss</Link></li>
-                    <li><Link to='/bixd/linde-helene-stormo'>Linde Stormo</Link></li>
-                    <li><Link to='/bixd/michelle-carina-osnes'>Michelle Carina Osnes</Link></li>
-                    <li><Link to='/bixd/sine-uberg'>Sine Uberg</Link></li>
-                </ul>
+          <h2>Informasjonsarkitektur-ansvarlige</h2>
+          <ul>
+            <li>
+              <Link to="/bixd/hanna-hernaes">Hanna Hernæs</Link>
+            </li>
+            <li>
+              <Link to="/bixd/ida-emilie-herfoss">Ida Emilie Herfoss</Link>
+            </li>
+            <li>
+              <Link to="/bixd/linde-helene-stormo">Linde Stormo</Link>
+            </li>
+            <li>
+              <Link to="/bixd/michelle-carina-osnes">
+                Michelle Carina Osnes
+              </Link>
+            </li>
+            <li>
+              <Link to="/bixd/sine-uberg">Sine Uberg</Link>
+            </li>
+          </ul>
 
-                <h2>Innholdsansvarlige</h2>
-                <ul>
-                    <li><Link to='/bixd/ann-margit-krog'>Ann Margit Krog</Link></li>
-                    <li><Link to='#'>Andrea Kristoffersen</Link></li>
-                    <li><Link to='/bwu/kristian-teppan'>Kristian Teppan</Link></li>
-                    <li><Link to='/bixd/lisa-sofie-edelman-saether'>Lisa Sofie Edelman Sæther</Link></li>
-                    <li><Link to='/bwu/sandra-kristiansen-smaaberg'>Sandra Kristiansen Smaaberg</Link></li>
-                    <li><Link to='/bwu/simen-tokerod-bergo'>Simen Tokerød Bergo</Link></li>
-                    <li><Link to='/bixd/thomas-hoiby'>Thomas Høiby</Link></li>
-                </ul>
+          <h2>Innholdsansvarlige</h2>
+          <ul>
+            <li>
+              <Link to="/bixd/ann-margit-krog">Ann Margit Krog</Link>
+            </li>
+            <li>
+              <Link to="#">Andrea Kristoffersen</Link>
+            </li>
+            <li>
+              <Link to="/bwu/kristian-teppan">Kristian Teppan</Link>
+            </li>
+            <li>
+              <Link to="/bixd/lisa-sofie-edelman-saether">
+                Lisa Sofie Edelman Sæther
+              </Link>
+            </li>
+            <li>
+              <Link to="/bwu/sandra-kristiansen-smaaberg">
+                Sandra Kristiansen Smaaberg
+              </Link>
+            </li>
+            <li>
+              <Link to="/bwu/simen-tokerod-bergo">Simen Tokerød Bergo</Link>
+            </li>
+            <li>
+              <Link to="/bixd/thomas-hoiby">Thomas Høiby</Link>
+            </li>
+          </ul>
 
-                <h2>Utviklingsansvarlige</h2>
-                <ul>
-                    <li><Link to='/bwu/cornelius-ottar-sandmael'>Cornelius Ottar Sandmæl</Link></li>
-                    <li><Link to='/bwu/glenn-eirik-hansen'>Glenn Eirik Hansen</Link></li>
-                    <li><Link to='/bwu/jostein-o-tollefsrud'>Jostein Ødegårdstuen Tollefsrud</Link></li>
-                </ul>
+          <h2>Utviklingsansvarlige</h2>
+          <ul>
+            <li>
+              <Link to="/bwu/cornelius-ottar-sandmael">
+                Cornelius Ottar Sandmæl
+              </Link>
+            </li>
+            <li>
+              <Link to="/bwu/glenn-eirik-hansen">Glenn Eirik Hansen</Link>
+            </li>
+            <li>
+              <Link to="/bwu/jostein-o-tollefsrud">
+                Jostein Ødegårdstuen Tollefsrud
+              </Link>
+            </li>
+          </ul>
 
-                <h2>Stilarkansvarlige</h2>
-                <ul>
-                    <li><Link to='/bwu/emil-boen'>Emil Bøen</Link></li>
-                    <li><Link to='/bwu/ida-m-r-gjeitsund'>Ida M. R. Gjeitsund</Link></li>
-                    <li><Link to='/bwu/lena-amdal-larsen'>Lena Amdal-Larsen</Link></li>
-                    <li><Link to='/bwu/leonard-avdullahu'>Leonard Avdullahu</Link></li>
-                    <li><Link to='/bwu/vilde-gunnes-bertelsen'>Vilde Gunnes Bertelsen</Link></li>
-                </ul>
-            </section>
-        </Layout>
-    );
+          <h2>Stilarkansvarlige</h2>
+          <ul>
+            <li>
+              <Link to="/bwu/emil-boen">Emil Bøen</Link>
+            </li>
+            <li>
+              <Link to="/bwu/ida-m-r-gjeitsund">Ida M. R. Gjeitsund</Link>
+            </li>
+            <li>
+              <Link to="/bwu/lena-amdal-larsen">Lena Amdal-Larsen</Link>
+            </li>
+            <li>
+              <Link to="/bwu/leonard-avdullahu">Leonard Avdullahu</Link>
+            </li>
+            <li>
+              <Link to="/bwu/vilde-gunnes-bertelsen">
+                Vilde Gunnes Bertelsen
+              </Link>
+            </li>
+          </ul>
+        </section>
+      </div>
+    </Layout>
+  )
 }
 
 export default About
