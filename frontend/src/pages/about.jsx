ec2dd968b93d22fcce4f25af4d89cdc3e33b3219
Glenn Hansen <hansenglenneirik@gmail.com>
2022-04-05 12:04:43 +0200

    Add links to about page, missing 1 student

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -17,55 +17,59 @@ function About() {
             <section className={styles.section}>
                 <h2>Bærekraftansvarlig</h2>
                 <ul>
-                    <li><Link to='#'>Tom Schrier</Link></li>
+                    <li><Link to='/bwu/tom-schrier'>Tom Schrier</Link></li>
                 </ul>
 
                 <h2>Designansvarlige</h2>
                 <ul>
-                    <li><Link to='#'>Adam Maximilian Stoffel</Link></li>
-                    <li><Link to='#'>Amalie Bach</Link></li>
-                    <li><Link to='#'>Andrea Hauge Sandvik</Link></li>
-                    <li><Link to='#'>Johanne Lyster Strande</Link></li>
-                    <li><Link to='#'>Martine Egeli</Link></li>
+                    <li><Link to='/bmed/adam-maximilian-stoffel'>Adam Maximilian Stoffel</Link></li>
+                    <li><Link to='/bmed/amalie-bach'>Amalie Bach</Link></li>
+                    <li><Link to='/bmed/andrea-hauge-sandvik'>Andrea Hauge Sandvik</Link></li>
+                    <li><Link to='/bmed/johanne-lyster-strande'>Johanne Lyster Strande</Link></li>
+                    <li><Link to='/bmed/martine-egeli'>Martine Egeli</Link></li>
                 </ul>
 
                 <h2>Fotoansvarlige</h2>
                 <ul>
-                    <li><Link to='#'>Johanne Lyster Strande</Link></li>
-                    <li><Link to='#'>Marc Daniel Einebakken</Link></li>
-                    <li><Link to='#'>Nora Strøm Myklebust</Link></li>
+                    <li><Link to='/bmed/johanne-lyster-strande'>Johanne Lyster Strande</Link></li>
+                    <li><Link to='/bmed/marc-daniel-einebakken'>Marc Daniel Einebakken</Link></li>
+                    <li><Link to='/bmed/nora-strom-myklebust'>Nora Strøm Myklebust</Link></li>
                 </ul>
 
                 <h2>Informasjonsarkitektur-ansvarlige</h2>
                 <ul>
-                    <li><Link to='#'>Hanna Hernæs</Link></li>
-                    <li><Link to='#'>Ida Emilie Herfoss</Link></li>
-                    <li><Link to='#'>Linde Stormo</Link></li>
-                    <li><Link to='#'>Michelle Carina Osnes</Link></li>
-                    <li><Link to='#'>Sine Uberg</Link></li>
+                    <li><Link to='/bixd/hanna-hernaes'>Hanna Hernæs</Link></li>
+                    <li><Link to='/bixd/ida-emilie-herfoss'>Ida Emilie Herfoss</Link></li>
+                    <li><Link to='/bixd/linde-helene-stormo'>Linde Stormo</Link></li>
+                    <li><Link to='/bixd/michelle-carina-osnes'>Michelle Carina Osnes</Link></li>
+                    <li><Link to='/bixd/sine-uberg'>Sine Uberg</Link></li>
                 </ul>
 
                 <h2>Innholdsansvarlige</h2>
                 <ul>
-                    <li><Link to='#'>Ann Margit Krog</Link></li>
+                    <li><Link to='/bixd/ann-margit-krog'>Ann Margit Krog</Link></li>
                     <li><Link to='#'>Andrea Kristoffersen</Link></li>
-                    <li><Link to='#'>Lisa Sofie Edelman Sæther</Link></li>
+                    <li><Link to='/bwu/kristian-teppan'>Kristian Teppan</Link></li>
+                    <li><Link to='/bixd/lisa-sofie-edelman-saether'>Lisa Sofie Edelman Sæther</Link></li>
+                    <li><Link to='/bwu/sandra-kristiansen-smaaberg'>Sandra Kristiansen Smaaberg</Link></li>
+                    <li><Link to='/bwu/simen-tokerod-bergo'>Simen Tokerød Bergo</Link></li>
+                    <li><Link to='/bixd/thomas-hoiby'>Thomas Høiby</Link></li>
                 </ul>
 
                 <h2>Utviklingsansvarlige</h2>
                 <ul>
-                    <li><Link to='#'>Cornelius Ottar Sandmæl</Link></li>
-                    <li><Link to='#'>Glenn Eirik Hansen</Link></li>
-                    <li><Link to='#'>Jostein Ødegårdstuen Tollefsrud</Link></li>
+                    <li><Link to='/bwu/cornelius-ottar-sandmael'>Cornelius Ottar Sandmæl</Link></li>
+                    <li><Link to='/bwu/glenn-eirik-hansen'>Glenn Eirik Hansen</Link></li>
+                    <li><Link to='/bwu/jostein-o-tollefsrud'>Jostein Ødegårdstuen Tollefsrud</Link></li>
                 </ul>
 
                 <h2>Stilarkansvarlige</h2>
                 <ul>
-                    <li><Link to='#'>Emil Bøen</Link></li>
-                    <li><Link to='#'>Ida M. R. Gjeitsund</Link></li>
-                    <li><Link to='#'>Lena Amdal-Larsen</Link></li>
-                    <li><Link to='#'>Leonard Avdullahu</Link></li>
-                    <li><Link to='#'>Vilde Gunnes Bertelsen</Link></li>
+                    <li><Link to='/bwu/emil-boen'>Emil Bøen</Link></li>
+                    <li><Link to='/bwu/ida-m-r-gjeitsund'>Ida M. R. Gjeitsund</Link></li>
+                    <li><Link to='/bwu/lena-amdal-larsen'>Lena Amdal-Larsen</Link></li>
+                    <li><Link to='/bwu/leonard-avdullahu'>Leonard Avdullahu</Link></li>
+                    <li><Link to='/bwu/vilde-gunnes-bertelsen'>Vilde Gunnes Bertelsen</Link></li>
                 </ul>
             </section>
         </Layout>
